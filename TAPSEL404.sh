b9be012142e52d1d54aa04380a609fcb4d0b5b27
Jhosua Saut Maruli <49725122+LuciferXtools@users.noreply.github.com>
2019-10-08 15:00:22 +0700

    Update TAPSEL404.sh

--- TAPSEL404.sh
+++ TAPSEL404.sh
@@ -1,8 +1,12 @@
 echo "Script udah pindah"
+echo
 sleep 1
 echo "Tapi masih bisa di install kok"
+echo
 sleep 2
 echo "Silahkan Check di Channel Anonymous Central"
+echo
 echo "Sedang Membuka You Tube..."
 sleep 3
 am start https://youtu.be/rdzqkmFILXU
+clear
